16e8bb64e6ad679297c40387a9766aa77c4273ee
Felipercb <felipercborges@gmail.com>
2021-12-09 10:01:47 -0300

    controle

--- app/Http/Controllers/controleController.php
+++ app/Http/Controllers/controleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Agendamentos;
+use App\Models\agendamentos;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
