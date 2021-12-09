293c2d9f742a3cecb256997dff4cb2534115a86c
Felipercb <felipercborges@gmail.com>
2021-12-09 09:05:46 -0300

    userseed

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -18,9 +18,9 @@ class UserSeed extends Seeder
         
         DB::beginTransaction();
             User::create([
-                "name" => "Felipe Roberto",
-                "email" => "212382018@eniac.edu.br",
-                "password" => "1234",
+                "name" => "Felipe Borges",
+                "email" => "felipercborges@gmail.com",
+                "password" => "eyJpdiI6ImxJZjZyMHcxaTVGQ21VNk9xTW9hQWc9PSIsInZhbHVlIjoiT2ZuNThpZll0bVcwQW9aYkd6dElxSG40S3I5bmhNdkZSRkozQ3pFa1hZcz0iLCJtYWMiOiIzNWE1MjFkNGRiNTdjMGRjMDQwYjE5YTBiOWQyYjI0YTQwMmZhOWJmOWZiYTJjN2FkMDZmNWY5YTMzNDcwYzllIiwidGFnIjoiIn0=",
                 "name" => "Felipe Roberto",
                 "admin" => 1,
                 "suporte" => 1
