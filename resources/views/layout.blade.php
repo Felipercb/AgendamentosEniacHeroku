2c4bc914c39bb93d557b3acb4e2cd501a1bf9777
Felipercb <felipercborges@gmail.com>
2021-12-09 10:21:40 -0300

    ...

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,13 +48,14 @@
 
             @if ($admin)
             <div class="dropdown show">
-                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Controle
-                </a>
+              <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown link
+              </a>
             
               <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="#">Agendamentos</a>
-                <a class="dropdown-item" href="#">Suportes</a>
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
               </div>
             </div>
             @endif
